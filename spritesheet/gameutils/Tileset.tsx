930f38f36985e169aa57367ecefd22576f3adee7
DanRuckz <dan210993@hotmail.com>
2021-02-28 00:32:00 +0200

    Add files via upload

---
+++ spritesheet/gameutils/Tileset.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Tileset" tilewidth="25" tileheight="25" tilecount="23" columns="23">
+ <image source="Tileset.png" width="575" height="25"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="0"/>
+  <terrain name="New Terrain" tile="1"/>
+  <terrain name="New Terrain" tile="2"/>
+  <terrain name="New Terrain" tile="3"/>
+  <terrain name="New Terrain" tile="4"/>
+  <terrain name="New Terrain" tile="5"/>
+  <terrain name="New Terrain" tile="6"/>
+  <terrain name="New Terrain" tile="7"/>
+  <terrain name="New Terrain" tile="8"/>
+  <terrain name="New Terrain" tile="9"/>
+  <terrain name="New Terrain" tile="10"/>
+  <terrain name="New Terrain" tile="11"/>
+  <terrain name="New Terrain" tile="12"/>
+  <terrain name="New Terrain" tile="13"/>
+  <terrain name="New Terrain" tile="14"/>
+  <terrain name="New Terrain" tile="15"/>
+  <terrain name="New Terrain" tile="16"/>
+  <terrain name="New Terrain" tile="17"/>
+  <terrain name="New Terrain" tile="18"/>
+  <terrain name="New Terrain" tile="19"/>
+  <terrain name="New Terrain" tile="20"/>
+  <terrain name="New Terrain" tile="21"/>
+ </terraintypes>
+ <tile id="1" terrain="0,0,0,0"/>
+ <tile id="2" terrain="1,1,1,1"/>
+ <tile id="3" terrain="2,2,2,2"/>
+</tileset>
---
+++ spritesheet/gameutils/bigTiles.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="bigTiles" tilewidth="50" tileheight="50" tilecount="12" columns="12">
+ <image source="bigTiles.png" width="600" height="50"/>
+ <terraintypes>
+  <terrain name="pink" tile="0"/>
+  <terrain name="cyan" tile="1"/>
+  <terrain name="orange" tile="2"/>
+  <terrain name="B_orange" tile="3"/>
+  <terrain name="green" tile="4"/>
+  <terrain name="B_green" tile="5"/>
+  <terrain name="yellow" tile="6"/>
+  <terrain name="B_purple" tile="7"/>
+  <terrain name="blue" tile="8"/>
+  <terrain name="sky" tile="9"/>
+  <terrain name="purple" tile="10"/>
+  <terrain name="black" tile="11"/>
+ </terraintypes>
+ <tile id="1" terrain=",0,,"/>
+</tileset>
